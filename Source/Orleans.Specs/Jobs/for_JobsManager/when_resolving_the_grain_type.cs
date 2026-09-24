51f15cd7b2b4d8d0c6fbfee21cc74a1eee194dda
Einar <einari@me.com>
2026-09-24 10:38:52 +0200

    Drop a using the spec no longer needs

--- Source/Orleans.Specs/Jobs/for_JobsManager/when_resolving_the_grain_type.cs
+++ Source/Orleans.Specs/Jobs/for_JobsManager/when_resolving_the_grain_type.cs
@@ -2,7 +2,6 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 using System.CodeDom.Compiler;
-using System.Reflection;
 
 namespace Cratis.Orleans.Jobs.for_JobsManager;
 
